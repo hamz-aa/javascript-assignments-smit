968920a7597f5d3f5dc303f49fb40d131788425a
Hamza <jhamzaahmed@gmail.com>
2023-11-19 19:17:20 +0500

    upto q:10 completed

--- Assignment_31-34/assignment_31-34.js
+++ Assignment_31-34/assignment_31-34.js
@@ -28,12 +28,12 @@ const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
 // else document.write(`Last Days of Month<br>`);
 
 // Q:06
-// let millsecondsSince1970 = date.getTime();
-// let minutesSice1970 = (millsecondsSince1970 - date.getMinutes()) / (1000 * 60 * 60);
+// let millisecondsSince1970 = date.getTime();
+// let minutesSince1970 = (millisecondsSince1970 - date.getMinutes()) / (1000 * 60 * 60);
 
 // document.write(`Current Date : ${date}<br>`);
-// document.write(`Elapsed Milliseconds since January 1, 1970 : ${millsecondsSince1970}<br>`);
-// document.write(`Elapsed Minutes since January 1, 1970 : ${minutesSice1970}<br>`);
+// document.write(`Elapsed Milliseconds since January 1, 1970 : ${millisecondsSince1970}<br>`);
+// document.write(`Elapsed Minutes since January 1, 1970 : ${minutesSince1970}<br>`);
 
 // Q:07
 // let hours = date.getHours();
@@ -47,3 +47,20 @@ const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
 // document.write(`${laterDate}<br>`);
 
 // Q:09
+// const ramadan = new Date(2023, 2, 23);
+
+// let diffInTime = date.getTime() - ramadan.getTime();
+// let diffInDays = Math.floor(diffInTime / (1000 * 3600 * 24));
+
+// document.write(`${diffInDays} days have passed since 1st Ramadan, 2023<br>`);
+
+// Q:10
+// const givenDate = new Date(2015, 0, 1);
+// let diffInTime = date.getTime() - givenDate.getTime();
+// let diffInDays = Math.floor(diffInTime / (1000 * 3600 *24));
+// let diffInMinutes = diffInDays * 24 * 60;
+
+
+// document.write(`${diffInMinutes} minutes have been passed since beginning of 2015<br>`);
+
+// Q:11
