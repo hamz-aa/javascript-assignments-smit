781c49f79b3cc6dcaab50d19fd1e9138e213a36f
Hamza <jhamzaahmed@gmail.com>
2023-11-20 16:08:34 +0500

    completed assignment 35-38

--- Assignment_35-38/assignment_35-38.js
+++ Assignment_35-38/assignment_35-38.js
@@ -1,4 +1,3 @@
-
 // Chapter#35-38
 
 // Q:01
@@ -84,3 +83,115 @@
 // let end = parseInt(prompt('Enter end number : '));
 
 // displayCount(start, end)
+
+// Q:08
+// function hypotenuse(base, perpendicular) {
+//   function square(num) {
+//     return num ** 2;
+//   }
+
+//   return Math.sqrt(square(base) + square(perpendicular));
+// }
+
+// let base = parseInt(prompt("Enter Base : "));
+// let perpendicular = parseInt(prompt("Enter Perpendicular : "));
+// document.write(`Hypotenuse : ${hypotenuse(base, perpendicular)}<br>`);
+
+// Q:09
+// function areaOfRectangle(length, width) {
+//   return length * width;
+// }
+
+// let length = 5;
+// let width = 3;
+
+// document.write(`Length : ${length}<br>`);
+// document.write(`Width : ${width}<br>`);
+// document.write(`Area (Arguments as value) : ${areaOfRectangle(5, 3)}<br>`);
+// document.write(
+//   `Area (Arguments as Variables) : ${areaOfRectangle(length, width)}<br>`
+// );
+
+// Q:10
+// function isPalindrome(word) {
+//   let len = word.length;
+//   for (let i = 0; i < parseInt(len / 2); i++) {
+//     if (word.charAt(i) != word.charAt(len - (i + 1))) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// let word = prompt("Enter a word : ");
+// if (isPalindrome(word)) document.write(`${word} is palindrome.<br>`);
+// else document.write(`${word} is not palindrome.<br>`);
+
+// Q:11
+// function capitalizeSentence(sentence) {
+//   sentence = sentence[0].toUpperCase() + sentence.slice(1, sentence.length);
+//   let len = sentence.length;
+//   for (let i = 0; i < len; i++) {
+//     if (sentence[i] === " ") {
+//       sentence =
+//         sentence.slice(0, i + 1) +
+//         sentence[i + 1].toUpperCase() +
+//         sentence.slice(i + 2, len);
+//     }
+//   }
+//   return sentence;
+// }
+
+// let sentence = "the quick brown fox";
+// document.write(`Input String  : ${sentence}<br>`);
+// document.write(`Output String : ${capitalizeSentence(sentence)}<br>`);
+
+// Q:12
+// function longestWord(sentence) {
+//   let splitSentence = sentence.split(" ");
+//   let max = splitSentence[0];
+//   for (word of splitSentence) {
+//     if (word.length > max.length) max = word;
+//   }
+//   return max;
+// }
+
+// let sentence = "Web Development Tutorial";
+// document.write(`Input String : ${sentence}<br>`);
+// document.write(`Longest Word : ${longestWord(sentence)}<br>`);
+
+// Q:13
+// function countOccurrence(sentence, character) {
+//   let count = 0;
+//   for (let i = 0; i < sentence.length; i++) {
+//     if (sentence[i] == character) count++;
+//   }
+//   return count;
+// }
+
+// let sampleString = "JSResourceS.com";
+// let character = "o";
+
+// document.write(`Sample String : ${sampleString}<br>`);
+// document.write(`Character to Search : ${character}<br>`);
+// document.write(
+//   `Number of Occurrences of ${character} : ${countOccurrence(
+//     sampleString,
+//     character
+//   )}<br>`
+// );
+
+// Q:14
+// function calcCircumference(radius) {
+//   return 2 * Math.PI * radius;
+// }
+
+// function calcArea(radius) {
+//   return Math.PI * radius;
+// }
+
+// let radius = parseFloat(prompt("Enter radius of circle : "));
+
+// document.write(`Radius : ${radius}<br>`);
+// document.write(`Circumference : ${calcCircumference(radius)}<br>`);
+// document.write(`Area : ${calcArea(radius)}<br>`);
